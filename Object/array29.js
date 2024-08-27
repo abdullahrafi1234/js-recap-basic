de09643d7565092856deadaa6ccb32856f7d1c2e
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-08-28 02:17:40 +0600

    practice basic

--- Object/array29.js
+++ Object/array29.js
@@ -1,9 +1,16 @@
 console.log('hello')
 
- function highestScore(scores) {
-   
+function highestScore(scores) {
+    let max = scores[0]
+
+    for (var x = 1; x < scores.length; x++) {
+        if (max > scores[x]) {
+            max = scores[x]
+        }
+    }
 }
 
 
 let scores = [20, 34, 54, 20, 10]
 highestScore(scores)
+
