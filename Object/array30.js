3369a243390a79fb45475a239e3557edf55ff897
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-08-28 02:42:18 +0600

    practice basic

--- Object/array30.js
+++ Object/array30.js
@@ -1,14 +1,24 @@
 function highestRunScorer(batsman) {
     console.log(batsman)
-    var runScorer = batsman[0]
+    var runScorer = batsman[0][0];
+    var runScore = batsman[0][1];
 
-    for (x = 0; x < batsman.length; x++) {
-        for (y = 0; y < batsman.length; y++) {
+    for (x = 1; x < batsman.length; x++) {
+        // for (y = 0; y < batsman.length; y++) {
+
+        // }
+        if (runScore < batsman[x][1]) {
+            runScore = batsman[x][1];
+            runScorer = batsman[x][0]
+
 
         }
     }
+    return runScorer;
 }
 
 
 var scorer = [['Ashraful', 95], ['Shakib', 19], ['Mushfiq', 35], ['Rafi', 18]]
-highestRunScorer(scorer)
+
+var highestScore = highestRunScorer(scorer)
+console.log(highestScore)
