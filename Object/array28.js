266c96f42295ef5a9ab54ab21df23ed2b8d2f6d6
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-08-27 16:47:38 +0600

    practice basic

--- Object/array28.js
+++ Object/array28.js
@@ -4,5 +4,12 @@ console.log('array method')
 
 var names = ['arif', 'rafi', 'kolpona']
 
-names.splice(2, 1, 'jubair', 'rahim')  // 2 er mane hoilo 2 index theke add hbe name gula and 1 er mane hoilo sesh dik theke 1 ta element remove hbe
+//adding elements using splice
+
+names.splice(2, 1, 'jubair', 'rahim') 
+
+// 2 er mane hoilo 2 index theke add hbe name gula and 1 er mane hoilo sesh dik theke 1 ta element remove hbe
+console.log(names)
+
+names.splice(0, 2)
 console.log(names)
