c3e4794947a60d852ffc46a88a8bd46e7e6265c0
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-08-28 16:38:45 +0600

    practice basic

--- Object/DOM35.js
+++ Object/DOM35.js
@@ -4,7 +4,10 @@ let myHeading = document.getElementById('heading1')
 myHeading.innerHTML = ('Hello')
 
 let paragraph = document.getElementById("para")
-
 paragraph.innerHTML = ('Bye')
+
+let paragraph2 = document.getElementById("para2")
+paragraph2.innerHTML = ('Bye')
+
 
 
