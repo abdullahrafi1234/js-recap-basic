022583b2c529fdeff94ae3ed60525b49f28ca424
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-08-28 13:58:59 +0600

    practice basic

--- Object/mathGame33.js
+++ Object/mathGame33.js
@@ -1,3 +1,6 @@
+var numOfWin = 0
+var numOfLost = 0
+
 for (var x = 1; x <= 5; x++) {
     var guessNumber = parseInt(prompt('Enter a number from 1 to 5'))
 
@@ -5,6 +8,13 @@ for (var x = 1; x <= 5; x++) {
 
     if (guessNumber == random) {
         console.log("You have won")
+        numOfWin = numOfWin + 1
     }
-    else (console.log('You have lost, Your number was ' + guessNumber))
+    else {
+        numOfLost = numOfLost + 1;
+        console.log('You have lost, Your number was ' + guessNumber)
+    }
 }
+
+document.write('Number of Win = ' + numOfWin + '<br/>')
+document.write('Number of Lost = ' + numOfLost)
