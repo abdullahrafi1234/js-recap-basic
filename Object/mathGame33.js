725a40d4b21532362eead9a822a83891ac423e4f
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-08-28 13:54:37 +0600

    practice basic

--- Object/mathGame33.js
+++ Object/mathGame33.js
@@ -1,8 +1,10 @@
-var guessNumber = parseInt(prompt('Enter a number from 1 to 5'))
+for (var x = 1; x <= 5; x++) {
+    var guessNumber = parseInt(prompt('Enter a number from 1 to 5'))
 
-var random = Math.floor(Math.random()*4+1)
+    var random = Math.floor(Math.random() * 4 + 1)
 
-if(guessNumber == random){
-    console.log("You have won")
+    if (guessNumber == random) {
+        console.log("You have won")
+    }
+    else (console.log('You have lost, Your number was ' + guessNumber))
 }
-else(console.log('You have lost, Your number was ' + guessNumber)) 
