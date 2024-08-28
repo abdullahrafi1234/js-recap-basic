dcdf3edf58444e049aad5665347f142fc7e6ccc0
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-08-28 16:13:11 +0600

    practice basic

--- Object/dateObjecct34.js
+++ Object/dateObjecct34.js
@@ -9,3 +9,6 @@ console.log(month);  //month 1 digit kom dekhabe 0-11 obdi dekhabe
 
 var currentDate = date.getDate()
 console.log(currentDate);
+
+var currentDay = date.getDay()
+console.log(currentDay);
