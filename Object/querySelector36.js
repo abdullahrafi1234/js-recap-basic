4e9ac353b90cdbc3aa318105b544a97cf3831d73
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-02 18:47:33 +0600

    practice basic

--- Object/querySelector36.js
+++ Object/querySelector36.js
@@ -2,4 +2,4 @@
 //=--------Query Selector------ (class ba id er css sign dite hbe)
 
 // var query = document.querySelector('#p-id').innerHTML = "This is Query Changed"
-var query2 = document.querySelector('p').innerHTML = "This is Query Changed by tag"
+var query2 = document.querySelector('p').innerHTML = "This Query Changed by tag"
