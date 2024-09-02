62ecfa9d2e8c85cc7059c3d89889abd4ddcb327b
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-02 19:57:38 +0600

    practice basic

--- Object/eventHandler37.js
+++ Object/eventHandler37.js
@@ -1,5 +1,9 @@
-function myMessage(){
+function myMessage1(){
     alert('Hello Everyone')
 }
 
 //onclick er modde myMessage() call kore dite hbe 
+
+function myMessage2(){
+    alert('Hello Everyone')
+}
