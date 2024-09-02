91df8ff1e8ea1437f6ab088726c465536e47ce85
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-02 19:58:18 +0600

    practice basic

--- Object/eventHandler37.js
+++ Object/eventHandler37.js
@@ -1,9 +1,9 @@
 function myMessage1(){
-    alert('Hello Everyone')
+    alert('Hello Everyone button one')
 }
 
 //onclick er modde myMessage() call kore dite hbe 
 
 function myMessage2(){
-    alert('Hello Everyone')
+    alert('Hello Everyone button two')
 }
