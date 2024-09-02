b439be524bc0fb73c93eee9f327486bfd24eb7b7
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-02 21:22:47 +0600

    practice basic

--- Object/domManipulation38.js
+++ Object/domManipulation38.js
@@ -11,5 +11,13 @@ var heading = document.createElement('h1')
 var text = document.createTextNode('This is Added Heading')
 heading.appendChild(text)
 
+//added a element
+
 var myDiv = document.getElementById('myDiv')
 myDiv.appendChild(heading)
+
+
+//remove element
+
+var heading2 = document.getElementsByTagName('h2')[0]
+myDiv.removeChild(heading2)
