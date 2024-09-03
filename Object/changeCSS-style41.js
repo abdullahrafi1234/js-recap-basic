d82140a6b1ecbcdb58102be8050d55951c874441
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-04 02:02:53 +0600

    practice basic

--- Object/changeCSS-style41.js
+++ Object/changeCSS-style41.js
@@ -1,10 +1,14 @@
+// function addStyle() {
+//     var myVar = document.getElementById('peraId')
+
+//     myVar.style.color = 'red';
+//     myVar.style.fontSize = '40px'
+// }
+
+//how to add css style on js code
 function addStyle() {
     var myVar = document.getElementById('peraId')
 
     myVar.style.color = 'red';
     myVar.style.fontSize = '40px'
-}
-
-function removeStyle () {
-    var myVar = document.getElementById('peraId')
 }
