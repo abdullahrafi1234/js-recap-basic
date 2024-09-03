ede3bd8c2cc0f82214c9dc2600ad61abbfc6ed29
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-04 02:04:30 +0600

    practice basic

--- Object/changeCSS-style41.js
+++ Object/changeCSS-style41.js
@@ -9,6 +9,5 @@
 function addStyle() {
     var myVar = document.getElementById('peraId')
 
-    myVar.style.color = 'red';
-    myVar.style.fontSize = '40px'
+    myVar.classList.add('para-style')
 }
