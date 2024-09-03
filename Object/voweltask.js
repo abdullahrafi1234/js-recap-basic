bf3249aa0974c92358e6f4c63bda29820ac16ee6
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-03 16:34:13 +0600

    practice basic

--- Object/voweltask.js
+++ Object/voweltask.js
@@ -1,7 +1,7 @@
 var checkVowel = ['a', 'e', 'i', 'o', 'u']
 var icon = prompt('Enter a letter')
 
-if(icon === checkVowel){
+if(icon == 'a', 'e', 'i', 'o', 'u'){
     console.log('Vowel')
 }
 else( console.log('Cons'))
