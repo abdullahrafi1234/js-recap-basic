7b551617f0caaa0fc41453d51ccf6c824ebd6fba
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-03 13:27:07 +0600

    practice basic

--- Object/image-slider40.js
+++ Object/image-slider40.js
@@ -1,11 +1,13 @@
 var photos = ['images/img1.jpg', 'images/img2.jpg', 'images/img3.jpg', 'images/img4.jpg']
 var imgTag = document.querySelector('img')
 
-function next(){
+var count = 0
+
+function next() {
 
 }
 
 
-function prev(){
+function prev() {
 
 }
