f7460427776f7aaf4559d1e760b5b7281ffcfd60
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-03 16:54:04 +0600

    practice basic

--- Object/voweltask.js
+++ Object/voweltask.js
@@ -1,7 +1,21 @@
 //Way 1
-var icon = prompt('Enter a letter')
 
-if(icon == 'a', 'e', 'i', 'o', 'u'){
+var letter = prompt('Enter a letter')
+
+if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
     console.log('Vowel')
 }
-else( console.log('Cons'))
+else {
+    console.log('Consonant')
+}
+
+//Way two
+
+var CheckLetter = 'b'
+
+if (CheckLetter == 'a' || CheckLetter == 'e' || CheckLetter == 'i' || CheckLetter == 'o' || CheckLetter == 'u') {
+    console.log('Vowel')
+}
+else {
+    console.log('Consonant')
+}
