acf711db75a1f698995c12b8d447ccb427c9806c
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-04 16:10:04 +0600

    practice basic

--- Object/multipleEvent43.js
+++ Object/multipleEvent43.js
@@ -1,4 +1,18 @@
-document.querySelectorAll('myButton')[0].addEventListener('click', function () {
 
-    document.querySelector('h2')
-})
+for(i=0; i<3; i++){
+    document.querySelectorAll('.myButton')[i].addEventListener('click', function () {
+        var text = this.innerHTML
+        document.querySelector('h2').innerHTML = 'Clicked by ' + text;  
+    })
+}
+
+
+// document.querySelectorAll('.myButton')[0].addEventListener('click', function () {
+//     var text = this.innerHTML
+//     document.querySelector('h2').innerHTML = 'Clicked by ' + text;  
+// })
+
+// document.querySelectorAll('.myButton')[1].addEventListener('click', function () {
+//     var text = this.innerHTML
+//     document.querySelector('h2').innerHTML = 'Clicked by ' + text;  
+// })
