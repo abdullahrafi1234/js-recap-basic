fc054618a2050dcc4cf26581b2f62291d0ffa996
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-04 16:33:03 +0600

    practice basic

--- Object/audioInJS44.js
+++ Object/audioInJS44.js
@@ -1,7 +1,17 @@
+// Play Audio
+
 for (i = 0; i < 3; i++) {
-    document.querySelectorAll('.myButton')[0].addEventListener('click', function () {
+    document.querySelectorAll('.myButton')[i].addEventListener('click', function () {
         var text = this.innerHTML
         console.log(text)
+        
+        switch(text){
+            case 'Button 1':
+                var audio = new Audio('sound1.mp3')
+                audio.play()
+                break;
+        }
+
     })
 }
 
