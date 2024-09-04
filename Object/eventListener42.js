841a9af45b278e3c9522cb96b6e66c2986729f03
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-04 15:55:57 +0600

    practice basic

--- Object/eventListener42.js
+++ Object/eventListener42.js
@@ -11,7 +11,12 @@ function myFunction() {
 document.getElementById('button2').addEventListener('click', function () { alert('Clicked by me') })
 
 
+//mouseover and mouseout
 var myVar = document.getElementById('my-style')
 myVar.addEventListener('mouseover', function () {
     myVar.classList.add('my-styles')
-})
+}) 
+myVar.addEventListener('mouseover', function () {
+    myVar.classList.add('my-styles')
+}) 
+
