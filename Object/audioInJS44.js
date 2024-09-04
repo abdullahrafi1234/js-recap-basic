8dca815ddf87a80fa021d08bb6d3ddd688f98259
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-04 16:36:20 +0600

    practice basic

--- Object/audioInJS44.js
+++ Object/audioInJS44.js
@@ -4,10 +4,18 @@ for (i = 0; i < 3; i++) {
     document.querySelectorAll('.myButton')[i].addEventListener('click', function () {
         var text = this.innerHTML
         console.log(text)
-        
-        switch(text){
+
+        switch (text) {
             case 'Button 1':
-                var audio = new Audio('sound1.mp3')
+                var audio = new Audio('sounds/sound1.mp3')
+                audio.play()
+                break;
+            case 'Button 2':
+                var audio = new Audio('sounds/sound2.mp3')
+                audio.play()
+                break;
+            case 'Button 3':
+                var audio = new Audio('sounds/sound3.mp3')
                 audio.play()
                 break;
         }
