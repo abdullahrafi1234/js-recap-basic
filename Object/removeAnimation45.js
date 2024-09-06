54165d8106731bc82aa60c5910c0bf36876e093b
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-06 12:48:27 +0600

    practice basic

---
+++ Object/removeAnimation45.js
@@ -0,0 +1,28 @@
+// Play Audio
+
+for (i = 0; i < 3; i++) {
+    document.querySelectorAll('.myButton')[i].addEventListener('click', function () {
+        var text = this.innerHTML
+        console.log(text)
+        audioPlay(text)
+    })
+}
+
+
+//same work with function
+function audioPlay(text) {
+    switch (text) {
+        case 'Button 1':
+            var audio = new Audio('sounds/sound1.mp3')
+            audio.play()
+            break;
+        case 'Button 2':
+            var audio = new Audio('sounds/sound2.mp3')
+            audio.play()
+            break;
+        case 'Button 3':
+            var audio = new Audio('sounds/sound3.mp3')
+            audio.play()
+            break;
+    }
+}
