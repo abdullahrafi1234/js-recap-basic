a5863bdece58cf6d3162829e3e2322072b1b2ccb
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-06 20:00:38 +0600

    practice basic

--- Object/Dom-Event47.js
+++ Object/Dom-Event47.js
@@ -1,3 +1,4 @@
 // DOM events
 // 1. Event Object
     //-- event type: change, submit, load, unload, open, play, canplay, pause, playing, progress, ended, resize, scroll, toggle etc.
+    //-- properties: type, target, preventDefault()
