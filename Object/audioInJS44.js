5f39f042ede878ce58eba671de4324212b9d7067
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-06 12:44:38 +0600

    practice basic

--- Object/audioInJS44.js
+++ Object/audioInJS44.js
@@ -5,21 +5,42 @@ for (i = 0; i < 3; i++) {
         var text = this.innerHTML
         console.log(text)
 
-        switch (text) {
-            case 'Button 1':
-                var audio = new Audio('sounds/sound1.mp3')
-                audio.play()
-                break;
-            case 'Button 2':
-                var audio = new Audio('sounds/sound2.mp3')
-                audio.play()
-                break;
-            case 'Button 3':
-                var audio = new Audio('sounds/sound3.mp3')
-                audio.play()
-                break;
-        }
+        audioPlay(text)
+
+        // switch (text) {
+        //     case 'Button 1':
+        //         var audio = new Audio('sounds/sound1.mp3')
+        //         audio.play()
+        //         break;
+        //     case 'Button 2':
+        //         var audio = new Audio('sounds/sound2.mp3')
+        //         audio.play()
+        //         break;
+        //     case 'Button 3':
+        //         var audio = new Audio('sounds/sound3.mp3')
+        //         audio.play()
+        //         break;
+        // }
     })
+}
+
+
+//same work with function
+function audioPlay (text){
+    switch (text) {
+        case 'Button 1':
+            var audio = new Audio('sounds/sound1.mp3')
+            audio.play()
+            break;
+        case 'Button 2':
+            var audio = new Audio('sounds/sound2.mp3')
+            audio.play()
+            break;
+        case 'Button 3':
+            var audio = new Audio('sounds/sound3.mp3')
+            audio.play()
+            break;
+    }
 }
 
 
