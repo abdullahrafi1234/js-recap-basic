cd0fdc76f4b92f78597ee8e5f17fdcd306683e1c
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-07 02:30:29 +0600

    practice basic

--- Object/onChangeEvent47.js
+++ Object/onChangeEvent47.js
@@ -1,9 +1,9 @@
 // DOM events
 // 1. Event Object
 
-    //-- event type: change, submit, load, unload, open, play, canplay, pause, playing, progress, ended, resize, scroll, toggle etc.
+//-- event type: change, submit, load, unload, open, play, canplay, pause, playing, progress, ended, resize, scroll, toggle etc.
 
-    //-- properties: type, target, preventDefault()
+//-- properties: type, target, preventDefault()
 
 // 2. MouseEvent Object
 // 3. KeyboardEvent Object
@@ -14,15 +14,17 @@
 
 
 
-function prepareJuice(fruit){
-    console.log('Juice made from '+ fruit)
-    const glass = fruit 
-    console.log(glass + ' Juice Poured into a glass')
-    return glass 
-      //return kore dile ans ta nicher serviceJuice variable er modde jaia thake tokhn console.log e ans dekha jay
+function prepareJuice(fruit) {
+    console.log('Juice made from ' + fruit)
+
+    const glass = fruit;
+    console.log(glass + ' Juice Poured into a glass');
+
+    return glass;
+    //return kore dile ans ta nicher serviceJuice variable er modde jaia thake tokhn console.log e ans dekha jay
 
 }
 
-var serviceJuice= prepareJuice('Mango')
-console.log('This is your '+serviceJuice + ' Juice')
+var serviceJuice = prepareJuice('Mango')
+console.log('This is your ' + serviceJuice + ' Juice')
 
