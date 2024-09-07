dab29e114f3f3b08a18cceedccabf530b739d50e
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-07 14:02:49 +0600

    practice js

--- Object/onSubmit48.js
+++ Object/onSubmit48.js
@@ -1,11 +1,13 @@
 const form = document.querySelector('form')
 console.log(form)
 const name = form.querySelector('div #name')
+const email = form.querySelector('div #email')
+const password = form.querySelector('div #password')
 console.log(name)
 
 
 
-function formHandler(e){
+function formHandler(e) {
     e.preventDefault()
-console.log('submit')
+    console.log('submit')
 }
