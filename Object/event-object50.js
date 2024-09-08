265a65a23931f0f441953e6c204a424aaa5128cc
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-08 13:58:26 +0600

    practice js

--- Object/event-object50.js
+++ Object/event-object50.js
@@ -4,21 +4,30 @@
 // 3. resize
 // 4. toggle
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     console.log('load')
 })
-window.addEventListener('unload', function(){
+window.addEventListener('unload', function () {
     console.log('unload')
 })
 
-window.addEventListener('scroll', function(){
+window.addEventListener('scroll', function () {
     console.log('scrolling')
 })
 
-window.addEventListener('resize', function(){
+window.addEventListener('resize', function () {
     console.log('resize')
     const width = window.outerWidth;
     const height = window.outerHeight;
 
     console.log(`height: ${height}, width: ${width}`)
+})
+
+const toggle = document.querySelector('details')
+
+toggle.addEventListener('toggle', function () {
+    console.log('toggle')
+})
+toggle.addEventListener('toggle', function (e) {
+    console.log(e.target.open)
 })
