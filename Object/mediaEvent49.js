deca57ad208f07dc176b4dc8c74291bbeb838fe9
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-08 13:27:49 +0600

    practice js

--- Object/mediaEvent49.js
+++ Object/mediaEvent49.js
@@ -1,3 +1,14 @@
 //  audio  / video events
 //  canplay, play, plying, pause, progress, ended, volumeChange, waiting
 
+const video = document.querySelector('video');
+
+video.addEventListener('canplay', function(){
+    console.log('can play')
+})
+video.addEventListener('play', function(){
+    console.log('play')
+})
+video.addEventListener('pause', function(){
+    console.log('paused')
+})
