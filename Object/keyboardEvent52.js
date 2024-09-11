9d5afce85479dbc79cfb9854b96b746ee3792ed5
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-11 16:30:32 +0600

    practice js

--- Object/keyboardEvent52.js
+++ Object/keyboardEvent52.js
@@ -3,3 +3,12 @@
 // 2. keypress (may not supported by some browsers) 
 // 3. keyup
 // 4. some properties - key, keyCode, code, shiftKey, cltKey, repeat
+
+var div = document.querySelector('textarea')
+div.addEventListener('keydown', function(){
+    console.log('keydown')
+})
+
+div.addEventListener('keyup', function(){
+    console.log('keydown')
+})
