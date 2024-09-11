405bb7e47992f92884b3272561779e123c569d1a
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-11 14:14:26 +0600

    practice js

--- Object/keyboardEvent52.js
+++ Object/keyboardEvent52.js
@@ -1,1 +1,5 @@
-//Keyboard Mouse
+//    Keyboard Mouse
+// 1. keydown - pressing a key, can repeat
+// 2. keypress (may not supported by some browsers) 
+// 3. keyup
+// 4. some properties - key, keyCode, code, shiftKey, cltKey, repeat
