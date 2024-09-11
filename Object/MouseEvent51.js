8dfda2f28d46948589d4b13ebc807754e9e82d17
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-11 13:55:48 +0600

    practice js

--- Object/MouseEvent51.js
+++ Object/MouseEvent51.js
@@ -10,12 +10,28 @@
 
 const div = document.querySelector('div')
 
-div.addEventListener('click', function(){
-    console.log('clicked')
+// div.addEventListener('click', function(){
+//     console.log('clicked')
+// })
+// div.addEventListener('dblclick', function(){
+//     console.log('clicked double ')
+// })
+// div.addEventListener('mousedown', function(){
+//     console.log('mouse down is mouse clicked')
+// })
+// div.addEventListener('mouseup', function(){
+//     console.log('mouse up is mouse click sere dibo')
+// })
+div.addEventListener('mouseenter', function () {
+    console.log('mouseenter mane oi element e mouse neoa')
 })
-div.addEventListener('dblclick', function(){
-    console.log('clicked double ')
+div.addEventListener('mouseleave', function () {
+    console.log('mouseleave mane oi element theke mouse ber kora ')
 })
-div.addEventListener('mo', function(){
-    console.log('clicked double ')
+
+div.addEventListener('mouseover', function () {
+    console.log('mouseover')
+})
+div.addEventListener('mousemove', function () {
+    console.log('mousemove')
 })
