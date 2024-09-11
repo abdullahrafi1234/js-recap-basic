7e0edb2ce75e09197fac3878304bc5c69f113238
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-11 13:42:49 +0600

    practice js

--- Object/MouseEvent51.js
+++ Object/MouseEvent51.js
@@ -1,2 +1,15 @@
+// 1. click
+// 2. dbclick
+// 3. 
+// 4. onmousedup
+// 5. onmouseenter
+// 6. onmouseleave
+// 7. onmousemove
+// 8. onmouseover
+
+
 const div = document.querySelector('div')
 
+div.addEventListener('click', function(){
+    console.log('clicked')
+})
