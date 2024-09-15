8caa37d262b7613e08d43cac22a3423f721921bd
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-15 21:00:33 +0600

    practice js

--- Object/61-handleError.js
+++ Object/61-handleError.js
@@ -1,15 +1,20 @@
 // Error handling -> try, catch, finally, throw
-
+// try , catch  shodo run korbe amn code e kaj kore
 
 try{
 
     //code test
     alert('hi');
-    alert(x)
-    alert('bye')
+    alert(x)  //error
+    // alert('bye')
 
 }catch(error){
 
     //handle error
     alert('Inside catch block')
+    console.log(error);
+}
+finally{
+    //je kono obsthay code kaj korbe
+    alert('bye')
 }
