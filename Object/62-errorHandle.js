685dfbdf4a20829545aa40379d96dde7e90dc638
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-15 21:12:20 +0600

    practice js

--- Object/62-errorHandle.js
+++ Object/62-errorHandle.js
@@ -1,4 +1,17 @@
 document.querySelector('#checkButton').addEventListener('click', function () {
+
     var num = document.querySelector('#textId').value
-    console.log(num);
+    // console.log(num);
+
+
+    try {
+        if (num < 5) {
+            throw 'input is too low'
+        }
+        else if (num > 10) {
+            throw 'input is too high'
+        }
+    } catch (error) {
+        console.log(error);
+    }
 })
