ce042d387aa4e99029859e6229338c1fd8e5550f
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-15 20:29:33 +0600

    practice js

--- Object/59-Create-A-Clock.js
+++ Object/59-Create-A-Clock.js
@@ -3,14 +3,25 @@ const message = document.querySelector('.message')
 
 saveButton.addEventListener('click', startClock)
 
-function startClock(){
+function startClock() {
     let date = new Date()
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let second = date.getSeconds();
-    let time ='Time: ' + hours + ':'+ minutes + ':' + second
+
+    minutes = formatTime(minutes)
+    second = formatTime(second)
+
+    let time = 'Time: ' + hours + ':' + minutes + ':' + second
 
     message.textContent = time;
 
     setInterval(startClock, 1000)
+}
+
+function formatTime(value) {
+    if (value < 10) {
+        value = '0' + value
+    }
+    return value
 }
