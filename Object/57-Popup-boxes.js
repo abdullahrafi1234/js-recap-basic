6cd332525000163c05de082696d5157715fc0783
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-15 19:45:09 +0600

    practice js

--- Object/57-Popup-boxes.js
+++ Object/57-Popup-boxes.js
@@ -5,4 +5,17 @@ console.log(window);
 // alert('this is alert BOM')
 
 // confirm box
-confirm('Are you sure')
+// confirm('Are you sure')
+
+function deleteSomething(){
+    let value = confirm('Do you want to delete?')
+
+    if (value){
+        console.log('Deleted')
+    }
+    else{
+        console.log('Not deleted')
+    }
+}
+
+deleteSomething();
