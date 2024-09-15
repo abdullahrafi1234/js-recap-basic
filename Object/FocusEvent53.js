e3f8a7cc8de50384c6ee0c9fd33198a875accc9e
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-15 13:18:11 +0600

    practice js

---
+++ Object/FocusEvent53.js
@@ -0,0 +1,8 @@
+//FocusEvent Object
+// <br>, <head> , <tittle>, <html>, <iframe> , <meta> , <pram> , <script> , <style> , <base>, <bdo> (agulay use hoyna focus)
+
+// 1. onblur
+// 2. onfocus
+// 3. onfocusin
+// 4. onfocusout
+
