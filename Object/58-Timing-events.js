2a66edcbee2fe8ecc5e929e9e18606b331f7a50f
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-15 20:14:17 +0600

    practice js

--- Object/58-Timing-events.js
+++ Object/58-Timing-events.js
@@ -31,9 +31,10 @@ saveButton.addEventListener('click', displayCount)
 
 function displayCount(){
     let count = 0;
-    message.textContent = count
+    message.textContent = count;
 
     setInterval(()=> {
-        
-    })
+        count++;
+        message.textContent = count;
+    }, 1000)
 }
