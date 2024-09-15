bdcc534eb0851667a86420c00968db206170322b
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-16 02:01:06 +0600

    practice es6

--- Object/66-default-rest-parameter.js
+++ Object/66-default-rest-parameter.js
@@ -6,6 +6,13 @@ function message(text = 'Hello this is default parameter') {
     console.log(text)
 }
 
-
 message()
 message('I love rainy day')
+
+
+function sum(x, y, ...z){
+    console.log(`${x},${y}, z = ${z}`)
+}
+
+sum(5, 15)
+sum(10, 20, 40, 30, 90)
