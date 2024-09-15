2f3366e6e6efc8ca71d9bbe6d51849f7e368938c
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-15 13:50:14 +0600

    practice js

--- Object/DragEvent55.js
+++ Object/DragEvent55.js
@@ -1,1 +1,15 @@
-// 
+// DragEvent Object
+// ondragstart
+// ondrag
+// ondragend
+// ondragenter
+// ondragover
+// ondragover
+// ondrop
+
+
+const p = document.querySelector('p')
+
+p.addEventListener('drag', function(){
+    console.log('on drag called')
+})
