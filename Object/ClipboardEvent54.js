1e810d9e615f9994fd7b07a8447adb51e2529193
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-15 13:43:21 +0600

    practice js

--- Object/ClipboardEvent54.js
+++ Object/ClipboardEvent54.js
@@ -2,3 +2,16 @@
 // oncopy
 // oncut
 // onpaste
+
+
+const input = document.querySelector('input')
+
+input.addEventListener('copy', function(){
+    console.log('you have copied')
+})
+input.addEventListener('cut', function(){
+    console.log('you have cut')
+})
+input.addEventListener('paste', function(){
+    console.log('you have pasted')
+})
