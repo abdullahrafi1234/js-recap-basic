a9459e0a48490a1c69d323d01779334d58c5546d
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-15 20:51:28 +0600

    practice js

--- Object/61-handleError.js
+++ Object/61-handleError.js
@@ -1,1 +1,15 @@
 // Error handling -> try, catch, finally, throw
+
+
+try{
+
+    //code test
+    alert('hi');
+    alert(x)
+    alert('bye')
+
+}catch(error){
+
+    //handle error
+    alert('Inside catch block')
+}
