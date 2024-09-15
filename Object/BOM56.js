e35f5c4fad757b3c4922d252e6aa51cd094e0e84
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-15 19:29:20 +0600

    practice js

--- Object/BOM56.js
+++ Object/BOM56.js
@@ -6,3 +6,8 @@ console.log(window.location);
 
 //href
 console.log(location.href)  //jei page e asi tar url
+console.log(location.protocol);
+console.log(location.hostname);
+console.log(location.host);
+console.log(location.port);
+console.log(location.pathname);
