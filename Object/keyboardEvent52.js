c5586e408e0ddf26fd9d2e1eebb95adcdc9065f1
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-15 13:14:32 +0600

    practice js

--- Object/keyboardEvent52.js
+++ Object/keyboardEvent52.js
@@ -5,17 +5,21 @@
 // 4. some properties - key, keyCode, code, shiftKey, cltKey, repeat
 
 var div = document.querySelector('textarea')
-// div.addEventListener('keydown', function(){
-//     console.log('keydown hosse keyboard e click kora')
-// })
-
-div.addEventListener('keyup', function(e){
-    console.log('keyup hosse keyboard e click sere deoa')
-    // console.log(e.keyCode);
-    // console.log(e.code);
-    // console.log(e.shiftKey);
-    console.log(e.ctrlKey);
+div.addEventListener('keydown', function(e){
+    // console.log('keydown hosse keyboard e click kora')
+    if(e.repeat){
+        console.log('repeat')
+        alert('do not repeat')  //keydown e repeat use kora jay
+    }
 })
+
+// div.addEventListener('keyup', function(e){
+//     console.log('keyup hosse keyboard e click sere deoa')
+//     // console.log(e.keyCode);
+//     // console.log(e.code);
+//     // console.log(e.shiftKey);
+//     // console.log(e.ctrlKey);
+// })
 
 // div.addEventListener('keypress', function(){
 //     console.log('keypress hosse just pess kora ')
