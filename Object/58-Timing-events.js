b72821975dfe165f0941abbd803a0b60d770fafc
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-15 20:03:12 +0600

    practice js

--- Object/58-Timing-events.js
+++ Object/58-Timing-events.js
@@ -2,3 +2,8 @@
 // Js Timing events methods
 // setTimeOut() ----->  ekta nirdishto time por kaj ta suro hbe
 // setInterval() -----> ekta nirdishto time por por kaj ta repeat hbe
+
+
+setTimeout(()=> {
+    console.log('hi');
+}, 2000)
