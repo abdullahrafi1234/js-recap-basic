9f70b6e3a1ff8021bf5ad5c9c1df07a3f8319c77
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-16 16:02:13 +0600

    practice es6

--- Object/69-for-of-for-in-loop.js
+++ Object/69-for-of-for-in-loop.js
@@ -1,7 +1,15 @@
 // for of and for in loop
 
 const names = ["s1", "s2", "s3", "s4"]
-
-for(let name of names){
+for(let name of names){  // for of use kora hoy array te
     console.log(name)
+}
+
+const objects = {
+    name: 'rafi',
+    id: 12,
+    cgpa: 3.54
+}
+for(let object in objects){  // for in use kora hoy object er modde
+    console.log(object)
 }
