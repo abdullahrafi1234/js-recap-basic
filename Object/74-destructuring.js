157c253beb54428184b5776b5b0dc37842f00fb8
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-18 02:14:45 +0600

    practice es6

--- Object/74-destructuring.js
+++ Object/74-destructuring.js
@@ -1,16 +1,19 @@
 // Destructuring array and objects
-document.getElementById('dlt-input').addEventListener('keyup', function(event) {
+document.getElementById('dlt-input').addEventListener('keyup', function (event) {
     const p = event.target.value;
+    console.log(p);
     const buttonDelete = document.getElementById('dlt-btn');
 
     if (p === 'delete') {
         buttonDelete.removeAttribute('disabled');
+
     } else {
         buttonDelete.setAttribute('disabled', true);
+
     }
 });
 
-document.getElementById('dlt-btn').addEventListener('click', function() {
+document.getElementById('dlt-btn').addEventListener('click', function () {
     const secret = document.getElementById('heading');
     secret.style.display = 'none'; // Correct way to hide the heading
 
