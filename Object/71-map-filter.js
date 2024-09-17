f491d9ddfefdfb9ace3d0bcc9c4a35fdfbd9cae8
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-17 12:33:59 +0600

    practice es6

--- Object/71-map-filter.js
+++ Object/71-map-filter.js
@@ -1,3 +1,15 @@
 // map and filter array function
 
 const numbers = [10, 20, 30, 40]
+
+// numbers.map((number)=> {
+//     console.log(number)
+// })
+
+numbers.map(number => {
+    console.log(number)
+})
+
+numbers.filter(num => {
+    console.log(num)
+})
