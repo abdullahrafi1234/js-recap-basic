75410f7b496e974db302fbf717bf7a077f6118ca
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-19 02:36:44 +0600

    practice es6

--- Object/80-promise.js
+++ Object/80-promise.js
@@ -1,4 +1,8 @@
 // js Promise
 
-// 1. How to create promise
+// 1. How to create promise => pending, resolve, reject
+const promise1 = new Promise((resolve, reject) => {
+
+})
+
 // 2. how to use promise
