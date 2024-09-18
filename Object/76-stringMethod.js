5a830e091f8d474ddc0316ecb095238c0a24e998
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-18 13:11:09 +0600

    practice es6

--- Object/76-stringMethod.js
+++ Object/76-stringMethod.js
@@ -1,7 +1,15 @@
 // startWith(searchString, position) => check a string starts with another string
 
+const message = 'Today is friday'
+console.log(message.startsWith('Today'));
+
 // endsWith(searchString, length) => check a string ends with another string
 
+console.log(message.endsWith('friday'));
+console.log(message.endsWith('Friday'));
+
 // includes(searchString, position) => check if a string contains another string
+
+console.log(message.includes('x'));
 
 // all these method are case sensitive
