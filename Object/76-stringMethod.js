f8041c91f33a73584278e70f79940e046f0594d3
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-18 13:09:22 +0600

    practice es6

--- Object/76-stringMethod.js
+++ Object/76-stringMethod.js
@@ -1,1 +1,7 @@
 // startWith(searchString, position) => check a string starts with another string
+
+// endsWith(searchString, length) => check a string ends with another string
+
+// includes(searchString, position) => check if a string contains another string
+
+// all these method are case sensitive
