953f940c2983e3760898092c5fdef976ebe092dc
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-18 12:50:56 +0600

    practice es6

--- Object/75-find-findIndex.js
+++ Object/75-find-findIndex.js
@@ -2,4 +2,7 @@
 // 1. find() ---> condition wise 1st value ta dibe jeita match korbe
 // 2. findIndex() ---> index number dibe condition wise
 
-const numbers = 
+const numbers =  [5, 20, 23, 40, 25]
+
+const firstEven = numbers.find(x=> x %2 ===0)
+console.log(firstEven);
