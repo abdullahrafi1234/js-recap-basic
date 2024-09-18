c2b05e98b3382200029b0a27a15d5d9010ae6da1
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-18 16:44:38 +0600

    practice es6

--- Object/78-sync-async.js
+++ Object/78-sync-async.js
@@ -8,3 +8,7 @@ console.log('Task4')
 // javascript synchronous programme
 
 // Synchronous programme hosse 1 ta sesh houar por onno arekta kaj suro hbe
+
+setTimeout() // eita ekta async function
+
+// async function seshe load hoy kinto eitaar jonno keo atkay thake na
