030a6753e175d0410f3e2e485461411f04710351
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-09-23 12:26:44 +0600

    practice js promise

--- Object/80-promise.js
+++ Object/80-promise.js
@@ -3,26 +3,27 @@
 
 // 1. How to create promise => pending, resolve, reject
 const promise1 = new Promise((resolve, reject) => {
-    let completedPromise = false;
+    let completedPromise = true;
 
-    if(completedPromise){
+    if (completedPromise) {
         resolve('Completed Promise 1')
     }
-    else{
+    else {
         reject('not completed promise 1')
     }
 
 })
 console.log(promise1);
 
- //function resolve hoile then er modde jabe
-promise1.then(res => { 
-    console.log(res);
-})
+//function resolve hoile then er modde jabe
+promise1
+    .then(res => {
+        console.log(res);
+    })
 
-promise1.catch(res => {
-    console.log(res);
-})
+    .catch(res => {
+        console.log(res);
+    })
 
 
 // 2. how to use promise 
